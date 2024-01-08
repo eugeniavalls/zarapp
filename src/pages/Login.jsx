1749d39cc3bce8861705b9284c1d1a1d9f502ef9
eugeniavalls <e.vallsalfaro@gmail.com>
2024-01-09 00:36:20 +0100

    enrutamiento

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,50 +1,55 @@
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { useEffect, useRef } from "react"
 
 export const Login = () => {
 
-    const {VITE_URL_API} = import.meta.env
+    const { VITE_URL_API } = import.meta.env
 
     const navigate = useNavigate()
     const nombre = useRef('')
     const pass = useRef('')
 
     //Si ya he iniciado sesión me redirige automaticamente al gestor
-    useEffect(()=>{
-        if(localStorage.getItem('usuarios')){
+    useEffect(() => {
+        if (localStorage.getItem('usuarios')) {
             navigate('/gestor')
-        } 
+        }
     })
 
-    const formHandler = (e) => {
+    const formHandler = async (e) => {
         e.preventDefault()
 
         let nuevo = {
-            nombre : nombre.current.value ,
-            pass : pass.current.value 
+            nombre: nombre.current.value,
+            pass: pass.current.value
         }
+
+        let controller = new AbortController()
 
         //Cambiando siempre a stringify
         let options = {
-            method : 'post', 
+            method: 'post',
             body: JSON.stringify(nuevo),
-            headers : {
-                "Content-type" : "application/json"
-            }
+            headers: {
+                "Content-type": "application/json"
+            },
+            signal: controller.signal
         }
-        fetch(VITE_URL_API, options) 
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
 
-            //Si los datos de inicio de sesión existen entonces navega al gestor
-            if (data){
-                localStorage.setItem('usuarios', JSON.stringify(data))
-                navigate('/gestor')
-            }
-        })
-        .catch(error => console.log(error))
-    } 
+        await fetch(VITE_URL_API, options)
+            .then(res => res.json())
+            .then(data => {
+                console.clear()
+                console.log(data)
+                //Si los datos de inicio de sesión existen entonces navega al gestor
+                if (data) {
+                    localStorage.setItem('usuarios', JSON.stringify(data))
+                    navigate('/gestor')
+                }
+            })
+            .catch(error => console.log(error))
+            .finally(() => controller.abort)
+    }
 
     return (
         <>
@@ -52,8 +57,8 @@ export const Login = () => {
 
             {/* Cuando se acceda a este formulario se enviará la información a la API y la Api tendrá que comprobar la informacion en la base de datos */}
             <form onSubmit={formHandler} >
-                <input type="text" name="nombre" ref={nombre}placeholder="user"/>
-                <input type="password" name="pass" ref={pass}placeholder="pass"/>
+                <input type="text" name="nombre" ref={nombre} placeholder="user" />
+                <input type="password" name="pass" ref={pass} placeholder="pass" />
                 <input type="submit" value="Acceder" />
             </form>
         </>
